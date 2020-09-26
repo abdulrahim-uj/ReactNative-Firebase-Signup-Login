27bdbdf439c6d1ef2b13faae160a9b1c26a1dad8
AbdulRahim <abdulrahim.uj@gmail.com>
2020-09-26 20:27:25 +0530

    Initial commit

---
+++ database/FirebaseDB.js
@@ -0,0 +1,17 @@
+// database/FirebaseDB.js
+
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxx-nynyx.firebaseapp.com",
+    databaseURL: "https://ccccxdegfd.firebaseio.com",
+    projectId: "ccxdxdx",
+    storageBucket: "sffedfgcbff.vvbghftv.com",
+    messagingSenderId: "1234567890",
+    appId: "7:1234567890:ced:12345erftghvbh0987654cdvbfghb"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
